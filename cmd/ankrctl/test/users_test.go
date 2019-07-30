6cd0ea6f23b89a7cf8cef564da8ebb0606117687
YifanLi0821 <yil150@eng.ucsd.edu>
2019-07-30 13:26:58 -0700

    [SWDEV-344] dccnCliTest

--- cmd/ankrctl/test/users_test.go
+++ cmd/ankrctl/test/users_test.go
@@ -1,19 +1,59 @@
+// +build !windows
+
+/*
+Copyright 2018 The Dccncli Authors All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package test
 
 import (
 	"fmt"
 	ankrctl "github.com/Ankr-network/dccn-cli"
 	"github.com/stretchr/testify/assert"
-	"math/rand"
 	"strings"
 	"testing"
-	"time"
 )
 
+const (
+	MockResultSuccess = "Success"
+	MockAppid        = "100"
+	MockAppName      = "app"
+	MockAppImage     = "nginx:1.12"
+	MockReplica       = "2"
+	MockUpdateImage   = "nginx:1.13"
+	MockUpdateReplica = "3"
+	MockAppType      = "Deploy"
+)
 
-func TestMockCommand(t *testing.T) {
+var lc = ankrctl.NewLiveCommand("../../../build/ankrctl_linux_amd64")
 
-	rand.Seed(time.Now().UnixNano())
+/*type mail struct {
+	From    string `json:"f"`
+	Subject string `json:"s"`
+	HTML    string `json:"html"`
+	Text    string `json:"text"`
+}
+
+type msg struct {
+	UID string `json:"uid"`
+}
+
+type inbox struct {
+	Msgs []msg `json:"msgs"`
+}*/
+
+func TestRunUserLogin(t *testing.T) {
+
+	/*rand.Seed(time.Now().UnixNano())
 	charsA := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	charsa := []rune("abcdefghijklmnopqrstuvwxyz")
 	nums := []rune("0123456789")
@@ -26,9 +66,8 @@ func TestMockCommand(t *testing.T) {
 		c.WriteRune(charsa[rand.Intn(len(charsa))])
 		c.WriteRune(charsa[rand.Intn(len(charsa))])
 		c.WriteRune(charsa[rand.Intn(len(charsa))])
-	}
+	}*/
 
-	lc := ankrctl.NewLiveCommand("../../../build/ankrctl_linux_amd64")
 	MockUserEmail := "test12345@mailinator.com"
 	MockPassword := "test12345"
 	fmt.Println("user login test..")
@@ -37,6 +76,5 @@ func TestMockCommand(t *testing.T) {
 		t.Error(err.Error())
 	}
 	fmt.Println(string(loginRes))
-	assert.True(t, strings.Contains(string(loginRes), "Success"))
-
+	assert.True(t, strings.Contains(string(loginRes), MockResultSuccess))
 }
